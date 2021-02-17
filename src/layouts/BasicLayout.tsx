6ce52319021f0f14ac489562de13bc7a56e1ccb6
conjuringwxq <1794594886@qq.com>
2021-02-17 17:20:33 +0800

    fix: 依赖更新

--- src/layouts/BasicLayout.tsx
+++ src/layouts/BasicLayout.tsx
@@ -1,19 +1,26 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import { RouteConfigComponentProps } from "react-router-config";
-import { Row, Col, Menu } from "antd";
+import { Layout, Breadcrumb, Row, Col, Menu } from "antd";
 import { MailOutlined } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import RouteView from "./RouteView";
 
+const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 const BasicLayout: FC<RouteConfigComponentProps> = (props) => {
   const { route } = props;
+  const [collapse, setCollapse] = useState(false);
+
+  const onCollapse = (val: boolean) => {
+    setCollapse(val);
+  }
 
   return (
-    <Row>
-      <Col>
-        <Menu theme="dark" mode="inline" style={{ width: 256 }}>
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider collapsible collapsed={collapse} onCollapse={onCollapse}>
+        <div style={{ height: "32px", margin: "16px", background: "rgba(255, 255, 255, 0.3)" }}>notice</div>
+        <Menu theme="dark" mode="inline">
           {route?.routes?.map((item: any) =>
             item.routes ? (
               <SubMenu key={item.path as string} icon={<MailOutlined />} title={item.title}>
@@ -30,11 +37,18 @@ const BasicLayout: FC<RouteConfigComponentProps> = (props) => {
             )
           )}
         </Menu>
-      </Col>
-      <Col>
-        <RouteView {...props} />
-      </Col>
-    </Row>
+      </Sider>
+      <Layout>
+        <Header>123</Header>
+        <Content style={{ margin: "0 16px" }}>
+          <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <RouteView {...props} />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 
