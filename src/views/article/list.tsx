11b141fd4e5c3d525af2c259f5ab506694bd8fde
conjuringwxq <1794594886@qq.com>
2021-02-21 23:23:21 +0800

    feat: 发布状态切换

--- src/views/article/list.tsx
+++ src/views/article/list.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useState, useEffect } from "react";
-import { Table, Space, Card, Form, Input, Button, Row, Col, Select, DatePicker } from "antd";
+import { Table, Space, Card, Form, Input, Button, Row, Col, Select, DatePicker, Tag, Switch } from "antd";
 import { useForm } from "antd/lib/form/Form";
 import axios from "axios";
 
@@ -36,27 +36,51 @@ const ArticleList: FC = () => {
   const [loading, setLoading] = useState(false);
   const [dataSource, setDataSource] = useState<Array<Article>>([]);
 
-  // 查询
+  /**
+   * @description 查询
+   * @param values 
+   */
   const onSearch = (values: any) => {
     queryList(values);
   };
 
-  // 重置
+  /**
+   * @description 重置
+   */
   const onReset = () => {
     form.resetFields();
   };
 
+  /**
+   * @description 查询列表
+   * @param params 
+   */
   const queryList = async (params?: any) => {
     setLoading(true);
     try {
       console.log(params);
       const fetchData = await axios.get("/api/article/list");
+      console.log(fetchData);
       setDataSource(fetchData.data.data);
     } catch (error) {
       console.log(error);
     } finally {
       setLoading(false);
     }
+  };
+
+  /**
+   * @description 发布状态切换监听事件
+   * @param val 
+   * @param row 
+   */
+  const onSwitchReleaseStatus = (val: boolean, row: Article) => {
+    const newData = [...dataSource];
+    const target = newData.find((item) => item.articleId === row.articleId);
+    if (target) {
+      target.releaseStatus = val ? 1 : 0;
+    }
+    setDataSource(newData);
   };
 
   useEffect(() => {
@@ -152,15 +176,73 @@ const ArticleList: FC = () => {
           </Row>
         </Form>
       </Card>
-      <Table<Article> bordered dataSource={dataSource} loading={loading}>
+      <Table<Article> bordered dataSource={dataSource} loading={loading} rowKey={(record) => record.articleId}>
         <Table.Column<Article> title="文章标题" dataIndex="title" align="center"></Table.Column>
         <Table.Column<Article> title="文章作者" dataIndex="author" align="center"></Table.Column>
-        <Table.Column<Article> title="关键字" dataIndex="keywords" align="center"></Table.Column>
+        <Table.Column<Article>
+          title="关键字"
+          dataIndex="keywords"
+          align="center"
+          render={(keywords: string[]) => (
+            <>
+              {keywords.map((keyword, index) => (
+                <Tag color="blue" key={keyword + index}>
+                  {keyword}
+                </Tag>
+              ))}
+            </>
+          )}
+        ></Table.Column>
         <Table.Column<Article> title="封面图" dataIndex="thumbUrl" align="center"></Table.Column>
-        <Table.Column<Article> title="文章标签" dataIndex="tag" align="center"></Table.Column>
-        <Table.Column<Article> title="文章分类" dataIndex="category" align="center"></Table.Column>
-        <Table.Column<Article> title="文章发布状态" dataIndex="releaseStatus" align="center"></Table.Column>
-        <Table.Column<Article> title="文章来源" dataIndex="source" align="center"></Table.Column>
+        <Table.Column<Article>
+          title="文章标签"
+          dataIndex="tag"
+          align="center"
+          render={(tag: string[]) => (
+            <>
+              {tag.map((item, index) => (
+                <Tag color="blue" key={item + index}>
+                  {item}
+                </Tag>
+              ))}
+            </>
+          )}
+        ></Table.Column>
+        <Table.Column<Article>
+          title="文章分类"
+          dataIndex="category"
+          align="center"
+          render={(category) => <Tag color="blue">{category}</Tag>}
+        ></Table.Column>
+        <Table.Column<Article>
+          title="文章发布状态"
+          dataIndex="releaseStatus"
+          align="center"
+          render={(releaseStatus, row) => (
+            <Switch
+              checkedChildren="已发布"
+              unCheckedChildren="未发布"
+              checked={releaseStatus === 1}
+              onChange={(checked) => onSwitchReleaseStatus(checked, row)}
+            />
+          )}
+        ></Table.Column>
+        <Table.Column<Article>
+          title="文章来源"
+          dataIndex="source"
+          align="center"
+          render={(source) =>
+            source === 1 ? (
+              <Tag color="purple">
+                <span>原创</span>
+              </Tag>
+            ) : (
+              <Tag color="orange">
+                <span>转载</span>
+              </Tag>
+            )
+          }
+        ></Table.Column>
         <Table.Column<Article> title="文章创建时间" dataIndex="createTime" align="center"></Table.Column>
         <Table.Column<Article> title="操作" align="center"></Table.Column>
       </Table>
