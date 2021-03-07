320dad62db887020b2b86e29f0877cad063395e7
wangxq02 <wangxq02@zenmen.com>
2021-03-07 19:19:44 +0800

    fix: 删除冗余配置

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,16 @@
 import { Button, Result } from 'antd';
 import React from 'react';
 import { history } from 'umi';
+import { HomeOutlined } from '@ant-design/icons';
 
 const NoFoundPage: React.FC = () => (
   <Result
     status="404"
     title="404"
-    subTitle="Sorry, the page you visited does not exist."
+    subTitle="对不起，页面未找到"
     extra={
-      <Button type="primary" onClick={() => history.push('/')}>
-        Back Home
+      <Button type="primary" icon={<HomeOutlined />} onClick={() => history.push('/')}>
+        返回首页
       </Button>
     }
   />
--- src/pages/article/List/index.tsx
+++ src/pages/article/List/index.tsx
@@ -1,7 +1,6 @@
 import { PageContainer } from '@ant-design/pro-layout';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Spin } from 'antd';
-import styles from './index.less';
 
 export default () => {
   const [loading, setLoading] = useState<boolean>(true);
@@ -11,7 +10,7 @@ export default () => {
     }, 3000);
   }, []);
   return (
-    <PageContainer content="这是一个新页面，从这里进行开发！" className={styles.main}>
+    <PageContainer>
       <div style={{ paddingTop: 100, textAlign: 'center' }}>
         <Spin spinning={loading} size="large" />
       </div>
